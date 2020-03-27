ffff8f7105c6302cacd3292d9927080d90bbf2f4
Brandon Spangler <brandonspangler@knights.ucf.edu>
2020-03-27 15:59:39 -0500

    problem 1 working

--- spangler_problem1/src/Application.java
+++ spangler_problem1/src/Application.java
@@ -1,4 +1,13 @@
 
 public class Application {
+	
+	public static void main(String[] args) {
+		
+		DuplicateRemover duplicateRemover = new DuplicateRemover();
+		
+		duplicateRemover.remove("problem1.txt");
+		
+		duplicateRemover.write("unique_words.txt");
+	}
 
 }
--- spangler_problem1/src/DuplicateRemover.java
+++ spangler_problem1/src/DuplicateRemover.java
@@ -1,4 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 
 public class DuplicateRemover {
-
+	
+	Set<String> uniqueWords = new HashSet<String>();
+	
+	public void remove(String dataFile) {
+		
+		File input = new File(dataFile);
+		
+		try (Scanner sc = new Scanner(input)) {
+			while (sc.hasNextLine()) {
+				uniqueWords.add(sc.next());
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void write(String outputFile) {
+		
+		File output = new File(outputFile);
+		
+		try (Writer write = new PrintWriter(output)) {
+			for (String word: uniqueWords) {
+				write.write(word);
+				write.write("\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
